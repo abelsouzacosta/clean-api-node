261bd5e816f10bef3c65cbfb9538fdaef51b540f
Abel Souza <abelsouzacosta@gmail.com>
2021-09-23 15:05:06 -0300

    test(password): loginRouter Password test
    Returns a statusCode 400 if a password does not provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -5,15 +5,34 @@ class LoginRouter {
         statusCode: 400
       }
     }
+
+    if (!httpRequest.body.password || httpRequest.body.password === '') {
+      return {
+        statusCode: 400
+      }
+    }
   }
 }
 
-describe('Login routerr', () => {
+describe('Login router', () => {
   test('Should return 400 if an email is not provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if an password is not provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: ''
       }
     }
     const httpResponse = sut.route(httpRequest)
