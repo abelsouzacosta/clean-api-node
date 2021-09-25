39777d789ad3a7a4ac746584770be88f629ccc29
Abel Souza <abelsouzacosta@gmail.com>
2021-09-25 01:33:37 -0300

    refactor(emailvalidator): move sut creator to a factory helper

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,16 +6,18 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => new EmailValidator()
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid@email.com')
 
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalidemail.com')
 
