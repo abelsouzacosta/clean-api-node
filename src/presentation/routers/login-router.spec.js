a889f2555a068bbbe91bd3d7ff6bb2e6e4acd184
Abel Souza <abelsouzacosta@gmail.com>
2021-09-25 00:55:20 -0300

    refactor(errors): exporting errors in a index file

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,5 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../errors/missing-param-error')
-const UnauthorizedError = require('../errors/unauthorized-error')
-const ServerError = require('../errors/server-error')
-const InvalidParamError = require('../errors/invalid-param-error')
+const { MissingParamError, InvalidParamError, UnauthorizedError, ServerError } = require('../errors')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
