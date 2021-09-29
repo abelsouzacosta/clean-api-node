ad74463a8083b861d18704222a560e7589a098ff
Abel Souza <abelsouzacosta@gmail.com>
2021-09-29 03:30:19 -0300

    refactor(authusecase): move dependencies validation for a separated method
    A specific method to dependencies validation was created

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,26 +1,30 @@
-const MissingParamError = require('../../utils/errors')
-const InvalidParamError = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
+const { InvalidParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   constructor (loadUserByEmailRepositorySpy) {
     this.loadUserByEmailRepositorySpy = loadUserByEmailRepositorySpy
   }
 
-  async auth (email, password) {
-    if (!email || email === '') {
-      throw new MissingParamError('email')
-    }
-
-    if (!password || password === '') {
-      throw new MissingParamError('password')
-    }
-
+  validateModuleDependencies () {
     if (!this.loadUserByEmailRepositorySpy) {
       throw new MissingParamError('loadUserByEmailRepositorySpy')
     }
 
     if (!this.loadUserByEmailRepositorySpy.load) {
       throw new InvalidParamError('loadUserByEmailRepositorySpy')
+    }
+  }
+
+  async auth (email, password) {
+    this.validateModuleDependencies()
+
+    if (!email || email === '') {
+      throw new MissingParamError('email')
+    }
+
+    if (!password || password === '') {
+      throw new MissingParamError('password')
     }
 
     const user = await this.loadUserByEmailRepositorySpy.load(email)
