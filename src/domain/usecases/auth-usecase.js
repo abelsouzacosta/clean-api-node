420cce7498374480783ec61e6013f75db3ebdf8f
Abel Souza <abelsouzacosta@gmail.com>
2021-09-29 18:14:51 -0300

    refactor(authusecase): simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -47,18 +47,14 @@ module.exports = class AuthUseCase {
 
     const user = await this.loadUserByEmailRepositorySpy.load(email)
 
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+
+      return accessToken
     }
 
-    const isValid = await this.encrypter.compare(password, user.password)
-
-    if (!isValid) {
-      return null
-    }
-
-    const accessToken = await this.tokenGenerator.generate(user.id)
-
-    return accessToken
+    return null
   }
 }
