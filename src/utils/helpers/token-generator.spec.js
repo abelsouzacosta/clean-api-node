19d75ecf886136dffa2a3d12112ab8543b2e08de
Abel Souza <abelsouzacosta@gmail.com>
2021-10-01 14:40:30 -0300

    test(tokengenerator): exception
    Ensure that token generator throws an exception if no id is provided for generate method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,21 +1,32 @@
-const makeSut = () => {
-  class TokenGenerator {
-    generate (id) {
-      return this.accessToken
-    }
-  }
+const MissingParamError = require('../errors/missing-param-error')
 
+class TokenGenerator {
+  generate (id) {
+    if (!id || id === '') {
+      throw new MissingParamError('id')
+    }
+
+    return this.accessToken
+  }
+}
+
+const makeSut = () => {
   const tokenGenerator = new TokenGenerator()
 
   return tokenGenerator
 }
 
 describe('Token Generator', () => {
-  it('Should return null if JWT returns null', async () => {
+  it('Should return null if JWT returns null', () => {
     const sut = makeSut()
     sut.accessToken = null
     const accessToken = sut.generate('any_id')
 
     expect(accessToken).toBeNull()
+  })
+
+  it('Should throw a new MissingParamException if an id is not provided', () => {
+    const sut = new TokenGenerator()
+    expect(sut.generate).toThrow(new MissingParamError('id'))
   })
 })
