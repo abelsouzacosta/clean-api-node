5c539ed5a5ce1554e7d0b6364c1b5757848a53bd
Abel Souza <abelsouzacosta@gmail.com>
2021-10-01 09:52:56 -0300

    refactor(encrypter): move encrypter creation to a factory function

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,16 +1,32 @@
-class Encrypter {
-  async compare (string, hash) {
-    this.string = string
-    this.hash = hash
-    return true
+const makeSut = () => {
+  class Encrypter {
+    async compare (string, hash) {
+      this.string = string
+      this.hash = hash
+      return this.isValid
+    }
   }
+
+  const sut = new Encrypter()
+  sut.isValid = true
+
+  return sut
 }
 
 describe('Encrypter', () => {
   it('Should return true when bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_password', 'hashed_password')
 
     expect(isValid).toBe(true)
+  })
+
+  it('Should return false when bcrypt returns false', async () => {
+    const sut = makeSut()
+    sut.isValid = false
+
+    const isValid = await sut.compare('any_password', 'imcompatible_hash')
+
+    expect(isValid).toBe(false)
   })
 })
