ea8584734194e7de02467d8bb44d1793df7858bf
Abel Souza <abelsouzacosta@gmail.com>
2021-10-01 17:25:44 -0300

    refactor(tokengenerator): add TokenGenerator production class

---
+++ src/utils/helpers/token-generator.js
@@ -0,0 +1,26 @@
+const MissingParamError = require('../errors/missing-param-error')
+const jwt = require('jsonwebtoken')
+
+module.exports = class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  validateConstructor () {
+    if (!this.secret || this.secret === '') {
+      throw new MissingParamError('secret')
+    }
+  }
+
+  generate (id) {
+    if (!id || id === '') {
+      throw new MissingParamError('id')
+    }
+
+    this.validateConstructor()
+
+    this.id = id
+
+    return jwt.sign(this.id, this.secret)
+  }
+}
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,29 +1,6 @@
 const MissingParamError = require('../errors/missing-param-error')
 const jwt = require('jsonwebtoken')
-
-class TokenGenerator {
-  constructor (secret) {
-    this.secret = secret
-  }
-
-  validateConstructor () {
-    if (!this.secret || this.secret === '') {
-      throw new MissingParamError('secret')
-    }
-  }
-
-  generate (id) {
-    if (!id || id === '') {
-      throw new MissingParamError('id')
-    }
-
-    this.validateConstructor()
-
-    this.id = id
-
-    return jwt.sign(this.id, this.secret)
-  }
-}
+const TokenGenerator = require('./token-generator')
 
 const makeSut = () => {
   const tokenGenerator = new TokenGenerator('secret')
