8de767c11117ad4673f217b29dfba53792f73bdf
Abel Souza <abelsouzacosta@gmail.com>
2021-10-03 01:21:16 -0300

    refactor(loaduserbyemailrepository): constructor
    Ensure that a new instance of LoadUserByEmailRepository must receive an userModel through the
    constructor parameter

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,28 +1,82 @@
 const { MissingParamError } = require('../../utils/errors')
+const { MongoClient } = require('mongodb')
 
 class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  validateConstructor () {
+    if (!this.userModel) {
+      throw new MissingParamError('userModel')
+    }
+  }
+
   async load (email) {
+    this.validateConstructor()
+
     if (!email || email === '') {
       throw new MissingParamError('email')
     }
 
-    return this.user
+    const user = this.userModel.findOne({ email })
+
+    return user
   }
 }
 
 describe('LoadUserByEmailRepostory', () => {
-  it('Should return null if an user is not found', async () => {
-    const sut = new LoadUserByEmailRepository()
-    sut.user = null
+  let client, db
+
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = client.db('')
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await client.close()
+  })
+
+  it('Should return null if no user is found', async () => {
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository(userModel)
+
     const user = await sut.load('invalid@mail.com')
 
     expect(user).toBeNull()
   })
 
-  it('Should throw a new MissinParamError if en email is not provided', async () => {
-    const sut = new LoadUserByEmailRepository()
+  it('Should return an user if user is found', async () => {
+    const userModel = db.collection('users')
+    await userModel.insertOne({
+      email: 'valid@mail.com'
+    })
+
+    const sut = new LoadUserByEmailRepository(userModel)
+    const user = await sut.load('valid@mail.com')
+
+    expect(user.email).toBe('valid@mail.com')
+  })
+
+  it('Should throw a new MissingParamError if an email is not provided', async () => {
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository(userModel)
     const promise = sut.load()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  it('Should throw a new MissingParamError if an userModel is not provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('valid@mail.com')
+
+    expect(promise).rejects.toThrow(new MissingParamError('userModel'))
   })
 })
