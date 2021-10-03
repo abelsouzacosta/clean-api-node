82a55d6c61a6c036d4adbaeb49cad081f4198c1c
Abel Souza <abelsouzacosta@gmail.com>
2021-10-03 00:26:09 -0300

    test(loaduserbyemailrepository): load method
    Should return null if an user is not found

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,5 +1,15 @@
-describe('LoadUserByEmailRepostory', () => {
-  it('Should return null if an user si not found', () => {
+class LoadUserByEmailRepository {
+  async load (email) {
+    return this.user
+  }
+}
 
+describe('LoadUserByEmailRepostory', () => {
+  it('Should return null if an user is not found', async () => {
+    const sut = new LoadUserByEmailRepository()
+    sut.user = null
+    const user = await sut.load('invalid@mail.com')
+
+    expect(user).toBeNull()
   })
 })
