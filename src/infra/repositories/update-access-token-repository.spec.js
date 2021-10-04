3dbd4025331b57667e96e3c536c572f1e58acdeb
Abel Souza <abelsouzacosta@gmail.com>
2021-10-04 07:51:43 -0300

    test(updateaccesstokenrepository): update method
    Ensure that the update method receives an userId

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,9 +1,13 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class UpdateAccessTokenRepository {
-  async update (token) {
+  async update (token, userId) {
     if (!token || token === '') {
       throw new MissingParamError('token')
+    }
+
+    if (!userId || userId === '') {
+      throw new MissingParamError('userId')
     }
   }
 }
@@ -14,5 +18,12 @@ describe('UpdateAccessTokenRepository', () => {
     const accessToken = sut.update()
 
     await expect(accessToken).rejects.toThrow(new MissingParamError('token'))
+  })
+
+  it('Should throw a new MissingParamError if no user id provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const accessToken = sut.update('any_token')
+
+    await expect(accessToken).rejects.toThrow(new MissingParamError('userId'))
   })
 })
