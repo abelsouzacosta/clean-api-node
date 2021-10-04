49f1730db1139c320a3c75dc0c7e31e3b5a86aac
Abel Souza <abelsouzacosta@gmail.com>
2021-10-04 08:08:53 -0300

    test(updateaccesstokenrepository): updateAccessTokenRepository constructor
    Ensure that the constructor receives an userModel

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,6 +1,18 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
+const MongoHelper = require('../helpers/mongo-helper')
+let db
 
 class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  validateConstructor () {
+    if (!this.userModel) {
+      throw new MissingParamError('userModel')
+    }
+  }
+
   async update (token, userId) {
     if (!token || token === '') {
       throw new MissingParamError('token')
@@ -9,10 +21,25 @@ class UpdateAccessTokenRepository {
     if (!userId || userId === '') {
       throw new MissingParamError('userId')
     }
+
+    this.validateConstructor()
   }
 }
 
 describe('UpdateAccessTokenRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.getDb()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   it('Should throw new MissingParamError if no token is provided', async () => {
     const sut = new UpdateAccessTokenRepository()
     const accessToken = sut.update()
@@ -25,5 +52,12 @@ describe('UpdateAccessTokenRepository', () => {
     const accessToken = sut.update('any_token')
 
     await expect(accessToken).rejects.toThrow(new MissingParamError('userId'))
+  })
+
+  it('Should throw a new MissingParamError if no userModel is provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const accessToken = sut.update('valid_token', 'valid_id')
+
+    await expect(accessToken).rejects.toThrow(new MissingParamError('userModel'))
   })
 })
