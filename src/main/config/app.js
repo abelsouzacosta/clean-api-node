e8c1d1ef2f39c55854f509b47c2238db4c463109
Abel Souza <abelsouzacosta@gmail.com>
2021-10-09 04:12:09 -0300

    chore(router): adding router
    Adding login-router file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const setupApp = require('./setup')
+const setupRoutes = require('./routes')
 
 setupApp(app)
+setupRoutes(app)
 
 app.get('/api/junior', (req, res) => {
   return res.status(200).send('junior')
