0cc9f47c5d1a9c0f0eb2820c20097764c0bd2a42
Abel Souza <abelsouzacosta@gmail.com>
2021-10-09 01:52:13 -0300

    test(contenttype): content-type xml
    Ensure that a xml will be returned if are necessary

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,15 @@ describe('Content Type', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  it('Should return xml content type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      return res.status(200).send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
