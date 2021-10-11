118a7952d813f0b3cd9bdd6c4fd592a82999b0bc
Abel Souza <abelsouzacosta@gmail.com>
2021-10-11 06:20:38 -0300

    refactor(index): using environment variables

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  tokenSecret: process.env.SECRET_KEY || 'secret'
+  tokenSecret: process.env.SECRET_KEY || 'secret',
+  port: process.env.PORT || 5858
 }
