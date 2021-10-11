c3c9c06c041b92c23c9346de08cc3d53065aabe9
Abel Souza <abelsouzacosta@gmail.com>
2021-10-11 06:32:35 -0300

    refactor(loginroutercomposer): returns a class

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,6 +1,7 @@
-const loginRouter = require('../composers/login-router-composer')
-const expressRouterAdapter = require('../adapters/express-router-adapter')
+const LoginRouterComposer = require('../composers/login-router-composer')
+const { adapt } = require('../adapters/express-router-adapter')
 
 module.exports = router => {
-  router.post('/login', expressRouterAdapter.adapt(loginRouter))
+  const loginRouterComposer = LoginRouterComposer.compose()
+  router.post('/login', adapt(loginRouterComposer))
 }
